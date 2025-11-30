a44129aa3ab71732bb0fa6e6065eb2f3129f6f5c
Suraj Yadav <ysuraj877@gmail.com>
2025-11-30 19:22:23 +0530

    feat: build indexes from source in fs to list proxy

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <QMimeDatabase>
 #include <QSortFilterProxyModel>
 #include <QFileSystemModel>
+#include <QStandardPaths>
+#include <QQueue>
 
 enum class FileType {
     Audio,
@@ -72,11 +74,13 @@ public:
     FStoListProxy(QObject *parent = nullptr):QAbstractProxyModel(parent) { }
 
     QModelIndex mapFromSource(const QModelIndex &sourceIndex) const {
-        return QAbstractProxyModel::mapFromSource(sourceIndex);
+        return QModelIndex();
+        // return QAbstractProxyModel::mapFromSource(sourceIndex);
     }
 
     QModelIndex mapToSource(const QModelIndex &proxyIndex) const {
-        return QAbstractProxyModel::mapToSource(proxyIndex);
+        return QModelIndex();
+        // return QAbstractProxyModel::mapToSource(proxyIndex);
     }
 
     QModelIndex parent(const QModelIndex &child) const {
@@ -92,7 +96,7 @@ public:
             return 0;
         }
 
-        return sourceModel()->rowCount();
+        return mVectorIndexes.length();
     }
 
     int columnCount(const QModelIndex &parent = QModelIndex()) const {
@@ -102,6 +106,77 @@ public:
 
         return 0;
     }
+
+    void buildIndex(const QString &directory){
+        qDebug() << "Directory " << directory;
+        auto *fsm = qobject_cast<QFileSystemModel*>(sourceModel());
+        if (!fsm){
+            return;
+        }
+
+        beginResetModel();
+        mVectorIndexes.clear();
+
+        QModelIndex directoryIndex = fsm->index(directory, 0);
+        int rowCount = fsm->rowCount(directoryIndex);
+        for (int r = 0; r < rowCount; r++){
+            QModelIndex index = sourceModel()->index(r, 0, directoryIndex);
+            mVectorIndexes.append(QPersistentModelIndex(index));
+        }
+
+        qDebug() << "Probe " << fsm;
+        qDebug() << "RowCount " << fsm->rowCount(directoryIndex);
+
+        endResetModel();
+    }
+private:
+    QVector<QPersistentModelIndex> mVectorIndexes;
+};
+
+class Names {
+public:
+    enum Name {
+        Directory,
+    };
+
+    static QString get(Name name){
+        switch (name){
+        case Directory:
+            return "Wallet";
+        default:
+            return "";
+        }
+    }
+};
+
+class StandardPaths {
+public:
+    enum Path {
+        RootPath,
+    };
+
+    static QString location(Path path){
+        switch(path){
+        case RootPath:
+        {
+            auto homePath = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+            auto home = QDir(homePath);
+            // FIXME - If I dont exist, create me
+            home.cd(Names::get(Names::Directory));
+            return home.absolutePath();
+        }
+        default:
+            return "";
+        }
+    }
+};
+
+
+class Settings {
+public:
+    static QString get(StandardPaths::Path path){
+        return StandardPaths::location(StandardPaths::RootPath);
+    }
 };
 
 int main(int argc, char *argv[])
@@ -109,10 +184,16 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     DroppablesWindow w;
     QFileSystemModel *fsModel = new QFileSystemModel(&w);
+    QString rootPath = Settings::get(StandardPaths::RootPath);
+    QModelIndex rootIndex = fsModel->setRootPath(rootPath);
     FStoListProxy *fsToList = new FStoListProxy(&w);
+    QObject::connect(
+        fsModel,
+        &QFileSystemModel::directoryLoaded,
+        fsToList,
+        &FStoListProxy::buildIndex
+    );
     fsToList->setSourceModel(fsModel);
-    QMimeDatabase mimeDb;
-    qDebug() << mimeDb.mimeTypeForFile("/home/jarusll");
     w.show();
     return a.exec();
 }
