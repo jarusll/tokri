b429ca6ec17a5206314e947efe3b79cc028ab7ef
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2025-12-11 17:38:09 +0530

    cleanup

--- themeprovider.h
+++ themeprovider.h
@@ -40,24 +40,6 @@ public:
         pal.setColor(QPalette::Link,          QColor("#005FB8"));
         pal.setColor(QPalette::LinkVisited,   QColor("#7B0796"));
 
-        // pal.setColor(QPalette::Window,           QColor(0xF9F9F9));
-        // pal.setColor(QPalette::WindowText,       QColor("#1F1F1F"));
-        // pal.setColor(QPalette::Base,             QColor("#FFFFFF"));
-        // pal.setColor(QPalette::AlternateBase,    QColor("#F3F3F3"));
-        // pal.setColor(QPalette::ToolTipBase,      QColor("#FFFFFF"));
-        // pal.setColor(QPalette::ToolTipText,      QColor("#000000"));
-        // pal.setColor(QPalette::PlaceholderText,  QColor("#6E6E6E"));
-        // pal.setColor(QPalette::Text,             QColor("#000000"));
-        // pal.setColor(QPalette::Button,           QColor("#F3F3F3"));
-        // pal.setColor(QPalette::ButtonText,       QColor("#000000"));
-        // pal.setColor(QPalette::BrightText,       QColor("#FF0000"));
-        // pal.setColor(QPalette::Highlight,        QColor("#0078D4"));
-        // pal.setColor(QPalette::Accent,           QColor("#0078D4"));
-        // pal.setColor(QPalette::HighlightedText,  QColor("#FFFFFF"));
-        // pal.setColor(QPalette::Link,             QColor("#0078D4"));
-        // pal.setColor(QPalette::LinkVisited,      QColor("#005A9E"));
-        // pal.setColor(QPalette::NoRole,           QColor(0, 0, 0, 0));
-
         return pal;
     };
 };
