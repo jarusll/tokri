3bf004d81b93063b607fe7f57086d6b2bce86fd5
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2025-12-12 15:09:21 +0530

    cleanup file names

--- filenameprovider.h
+++ filenameprovider.h
@@ -8,19 +8,18 @@
 
 class FileNameProvider : public QObject
 {
-    Q_OBJECT
 public:
 
     explicit FileNameProvider(QObject *parent = nullptr);
 
     static QString nameFromText(const QString &text){
         QString rootPath = Settings::get(StandardPaths::RootPath);
-        QString fileNameBase = text.left(100);
+        QString fileNameBase = text.left(80);
         // FIXME this should be declared once
-        static const QRegularExpression invalidFileNameChars(
-            R"([^A-Za-z0-9_.-])"
-        );
-        fileNameBase.replace(invalidFileNameChars, "_");
+        static const QRegularExpression forbidden(R"([<>:"/\\|?*\x00-\x1F])");
+        fileNameBase.replace(forbidden, "_");
+        while (fileNameBase.startsWith("_")) fileNameBase.remove(0, 1);
+        while (fileNameBase.endsWith("_")) fileNameBase.chop(1);
         fileNameBase.append(".txt");
         return QDir(rootPath).filePath(fileNameBase);
     }
@@ -31,10 +30,10 @@ public:
         QString rootPath = Settings::get(StandardPaths::RootPath);
         QString fileNameBase = baseName;
         // FIXME this should be declared once
-        static const QRegularExpression invalidFileNameChars(
-            R"([^A-Za-z0-9_.-])"
-        );
-        fileNameBase.replace(invalidFileNameChars, "_");
+        static const QRegularExpression forbidden(R"([<>:"/\\|?*\x00-\x1F])");
+        fileNameBase.replace(forbidden, "_");
+        while (fileNameBase.startsWith("_")) fileNameBase.remove(0, 1);
+        while (fileNameBase.endsWith("_")) fileNameBase.chop(1);
         return QDir(rootPath + "/" + fileNameBase).path();
     }
 };
