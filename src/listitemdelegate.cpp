ee48d3187bfbe81494ff50655c187b9812907e16
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2025-12-31 19:59:32 +0530

    add padding

--- src/listitemdelegate.cpp
+++ src/listitemdelegate.cpp
@@ -7,9 +7,11 @@
 #include <QApplication>
 
 namespace {
-    constexpr int iconPx = 90;
-    constexpr int iconTextGap = 4;
-    constexpr int gapPx  = 6;
+constexpr int iconPx = 90;
+constexpr int gapPx = 6;
+constexpr int iconTextGap = 4;
+constexpr int textBottomPadPx = 6;
+constexpr int textHorzPadPx = 4;
 }
 
 ListItemDelegate::ListItemDelegate() {}
@@ -18,10 +20,11 @@ QSize ListItemDelegate::sizeHint(const QStyleOptionViewItem &opt,
                                  const QModelIndex &) const
 {
     const QFontMetrics fm(opt.font);
-    const int textH = fm.height();
+    const int textH = fm.ascent() + fm.descent() + textBottomPadPx;
+
     return QSize(
         iconPx,
-        iconPx + gapPx + textH
+        gapPx + iconPx + iconTextGap + textH
         );
 }
 
@@ -53,23 +56,24 @@ void ListItemDelegate::paint(QPainter *p,
 
     // icon
     const QRect iconRect(
-        r.center().x() - iconPx/2,
+        r.center().x() - iconPx / 2,
         r.top() + gapPx,
         iconPx,
         iconPx
         );
 
-    QMimeDatabase db;
+    static QMimeDatabase db;
     if (db.mimeTypeForFile(fi).name().startsWith("image/")) {
-        QPixmap previewImage(fi.filePath());
-        if (!previewImage.isNull()) {
-            previewImage = previewImage.scaled(
+        QPixmap pm(fi.filePath());
+        if (!pm.isNull()) {
+            pm = pm.scaled(
                 iconRect.size(),
                 Qt::KeepAspectRatio,
                 Qt::SmoothTransformation);
             p->drawPixmap(
-                iconRect.center() - QPoint(previewImage.width() / 2, previewImage.height() / 2),
-                previewImage);
+                iconRect.center()
+                    - QPoint(pm.width() / 2, pm.height() / 2),
+                pm);
         }
     } else {
         idx.data(Qt::DecorationRole).value<QIcon>()
@@ -77,11 +81,14 @@ void ListItemDelegate::paint(QPainter *p,
     }
 
     // text
-    const QRect textRect(
-        r.left(),
+    const QFontMetrics fm(o.font);
+    const int textHeight = fm.ascent() + fm.descent();
+
+    QRect textRect(
+        r.left() + textHorzPadPx,
         iconRect.bottom() + iconTextGap,
-        r.width(),
-        o.fontMetrics.height()
+        r.width() - 2 * textHorzPadPx,
+        textHeight + textBottomPadPx
         );
 
     p->setPen(o.state & QStyle::State_Selected
@@ -89,12 +96,12 @@ void ListItemDelegate::paint(QPainter *p,
                   : o.palette.color(QPalette::Text));
 
     p->drawText(
-        textRect,
-        Qt::AlignHCenter | Qt::AlignVCenter,
-        o.fontMetrics.elidedText(
+        textRect.adjusted(0, 0, 0, -textBottomPadPx),
+        Qt::AlignHCenter | Qt::AlignBottom,
+        fm.elidedText(
             idx.data(Qt::DisplayRole).toString(),
             Qt::ElideMiddle,
-            r.width())
+            textRect.width())
         );
 
     p->restore();
