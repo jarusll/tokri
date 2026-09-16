ca011a7b561d608e3308728332da79de981b56fa
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-09-16 15:59:21 +0530

    force x11

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,9 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_LINUX
+    qputenv("QT_QPA_PLATFORM", "xcb");
+#endif
     QApplication a(argc, argv);
 
     a.setPalette(ThemeProvider::theme());
